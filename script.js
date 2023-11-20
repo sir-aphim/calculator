9eaa1b22297adf0b5583c663a255774f80234e19
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2023-11-20 17:12:18 -0300

    Add digit query selectors

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function subtract(a, b) {
 function divide(a, b) {
     a / b;
 }
+
+const digitOne = document.querySelector('one')
+const digitTwo = document.querySelector('two')
+const digitThree = document.querySelector('three')
+
+const plusSign = document.querySelector('plus')
