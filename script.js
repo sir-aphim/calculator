3d6e5e8c824db07623b1f8da5a48c001884e9a84
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2023-11-21 13:46:14 -0300

    Add event listeners

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+let a;
+let b;
+let operator;
+
 function add(a, b) {
-    a + b;
+    return a + b;
 }
 
 function multiply(a, b) {
-    a * b;
+    return a * b;
 }
 
 function subtract(a, b) {
-    a - b;
+    return a - b;
 }
 
 function divide(a, b) {
-    a / b;
+    return a / b;
 }
 
-const digitOne = document.querySelector('one')
-const digitTwo = document.querySelector('two')
-const digitThree = document.querySelector('three')
 
-const plusSign = document.querySelector('plus')
+const digitOne = document.querySelector('#one')
+const digitTwo = document.querySelector('#two')
+const digitThree = document.querySelector('#three')
+
+const plusSign = document.querySelector('#plus')
+const subtractSign = document.querySelector('#subtract')
+const equalSign = document.querySelector('#equals') 
+
+digitOne.addEventListener("click", () => {
+    
+});
+
+digitTwo.addEventListener("click", () => {
+
+});
+
+plusSign.addEventListener("click", () => {
+
+});
+
+subtractSign.addEventListener("click", () => {
+   
+})
+
+equalSign.addEventListener("click", () => {
+    console.log(operate(a, b));
+})
